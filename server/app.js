4e00115fc0652b48c43828cf51c2a2d3fe4062cc
tair18oz <tairoz@hilma.tech>
2024-11-27 12:47:39 +0200

    some changes

--- server/app.js
+++ server/app.js
@@ -1,41 +1,95 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+// var todoRouter = require("./routes/users/:username/todo");
+// var postRouter = require("./routes/users/:username/todo");
+// var commentRouter = require("./routes/users/:username/todo");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
+
+// var createError = require("http-errors");
+// var express = require("express");
+// var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
+
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
+// var todosRouter = require("./routes/todos");  // Import the todos router
+// var postsRouter = require("./routes/posts");  // Import the posts router
+// var commentsRouter = require("./routes/comments");  // Import the comments router
+
+// var app = express();
+
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
+
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // Define the routes
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+
+// // Use routes for todos, posts, and comments for specific users
+// app.use("/users/:userId/todos", todosRouter);
+// app.use("/users/:userId/posts", postsRouter);
+// app.use("/users/:userId/comments", commentsRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render("error");
+// });
+
+// module.exports = app;
