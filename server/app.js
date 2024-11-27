22717e99743e25fe25842e6b6f646a312ce7f631
naamaWyler <naamawyler@hilma.tech>
2024-11-27 13:23:58 +0200

    removed table constuctor func

--- server/app.js
+++ server/app.js
@@ -51,33 +51,4 @@ con.connect(function (err) {
   console.log("Connected!");
 });
 
-let tableFiles = ["comment", "ident", "post", "todo", "user"];
-
-async function createTables(fileArr) {
-  for (let i = 0; i < fileArr.length; i++) {
-    let route = `./entities/${fileArr[i]}.json`;
-    try {
-      const res = await fsPromise.readFile(route, "utf8");
-      let tableRows = JSON.parse(res);
-      let s = "";
-      for (let key in tableRows) {
-        s += `${key} ${tableRows[key]},`;
-      }
-      s = s.slice(0, -1);
-
-      let sql = `CREATE TABLE ${fileArr[i]} (${s})`;
-      console.log("sql: ", sql);
-
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-}
-
-createTables(tableFiles);
-
 module.exports = app;
